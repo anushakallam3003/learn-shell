c8ffda803cb7e4568d12da1183a4547c6b687833
AnushaKallam3003 <kallamanusha67@gmail.com>
2025-12-03 16:53:19 +0530

    variables intro

--- common.sh
+++ common.sh
@@ -1,3 +1,9 @@
 sample1() {
   echo Sample1 Function
 }
+
+sample2() {
+	b=20
+	echo sample2 function
+	echo a - $a
+}
--- function.sh
+++ function.sh
@@ -6,3 +6,7 @@ sample
 
 source common.sh
 sample1
+
+a=10
+sample2
+echo b - $b
