efe5d4168eab94162c793f6040a0e4a43024e98a
AnushaKallam3003 <kallamanusha67@gmail.com>
2025-12-05 14:50:22 +0530

    variables intro

---
+++ variables-inputs.sh
@@ -0,0 +1,14 @@
+echo x - $x
+
+
+# Position
+echo First Argument - $1
+echo Second Argument - $2
+echo All Arguments - $*
+echo No of Arguments - $#
+
+
+# read
+
+read -p 'Enter y: ' y
+echo y - $y
